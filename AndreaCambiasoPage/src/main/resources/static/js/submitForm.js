2c1d14f4aae55f1fba2fcf5cab8894079ccc35a2
sib002 <czzsmn02p07c349d@studenti.unical.it>
2024-11-02 15:39:48 +0100

    rimosso tutto dal remote

--- AndreaCambiasoPage/src/main/resources/static/js/submitForm.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    document.getElementById("askQuestion").addEventListener("submit",function (event) {
-        event.preventDefault();
-        alert("Form submitted successfully!");
-        document.getElementById("askQuestion").reset();
-    });
-});
